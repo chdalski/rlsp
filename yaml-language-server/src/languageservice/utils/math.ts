1208124d4878b6e92982d9745a10ffd4ee124763
Mr. Robot <mr.robot@chrisski.dev>
2026-03-16 14:14:21 +0000

    chore: remove the original yaml language server

--- yaml-language-server/src/languageservice/utils/math.ts
+++
@@ -1,8 +0,0 @@
-export function floatSafeRemainder(val: number, step: number): number {
-  const valDecCount = (val.toString().split('.')[1] || '').length;
-  const stepDecCount = (step.toString().split('.')[1] || '').length;
-  const decCount = Math.max(valDecCount, stepDecCount);
-  const valInt = parseInt(val.toFixed(decCount).replace('.', ''));
-  const stepInt = parseInt(step.toFixed(decCount).replace('.', ''));
-  return (valInt % stepInt) / Math.pow(10, decCount);
-}
